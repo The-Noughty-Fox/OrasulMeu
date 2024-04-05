40f11f26f87457faa9f02cf5a57f4667dab2ebfc
Serghei Tibulschi <serghei@thenoughtyfox.com>
2024-04-05 12:58:27 +0300

    update swagger

--- backend/src/resources/comment/comment.controller.ts
+++ backend/src/resources/comment/comment.controller.ts
@@ -1,33 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller('comment')
+@ApiTags('comments')
+@Controller('comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Post()
+  @ApiOperation({ operationId: 'create-comment' })
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentService.create(createCommentDto);
   }
 
   @Get()
+  @ApiOperation({ operationId: 'get-comments' })
   findAll() {
     return this.commentService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ operationId: 'get-comment' })
   findOne(@Param('id') id: string) {
     return this.commentService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ operationId: 'update-comment' })
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentService.update(+id, updateCommentDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ operationId: 'delete-comment' })
   remove(@Param('id') id: string) {
     return this.commentService.remove(+id);
   }
