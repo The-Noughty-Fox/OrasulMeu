0fd1b5e050f32dc08f9f23e6b2ff81b3f9737b30
Serghei Tibulschi <serghei@thenoughtyfox.com>
2024-04-05 14:31:29 +0300

    update swagger

--- backend/src/resources/media/dto/media.dto.ts
+++ backend/src/resources/media/dto/media.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class MediaDto {
-  @ApiProperty({ type: 'string', format: 'binary' })
+  @ApiProperty({ type: 'string', format: 'binary', isArray: true })
   files: any;
 }
