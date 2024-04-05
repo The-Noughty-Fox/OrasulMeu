d410ab6487a8a62f990bd144587f5a15c6e46908
Serghei Tibulschi <serghei@thenoughtyfox.com>
2024-04-05 13:10:05 +0300

    update swagger

--- backend/src/resources/media/entities/media.entity.ts
+++ backend/src/resources/media/entities/media.entity.ts
@@ -1,22 +1,27 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { AutoMap } from '@automapper/classes';
 import { MediaType } from '@/resources/media/types';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'media' })
 export class Media {
   @PrimaryGeneratedColumn()
   @AutoMap()
+  @ApiProperty({ type: 'integer' })
   id: number;
 
   @Column()
   @AutoMap()
+  @ApiProperty({ enum: MediaType })
   type: MediaType;
 
   @Column()
   @AutoMap()
+  @ApiProperty()
   url: string;
 
   @Column()
   @AutoMap()
+  @ApiProperty()
   fileName: string;
 }
--- backend/src/resources/post/dto/post.dto.ts
+++ backend/src/resources/post/dto/post.dto.ts
@@ -2,25 +2,34 @@ import { UserDto } from '@/resources/user/dto/user.dto';
 import { CommentDto } from '@/resources/comment/dto/comment.dto';
 import { AutoMap } from '@automapper/classes';
 import { Media } from '@/resources/media/entities/media.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PostDto {
   @AutoMap()
+  @ApiProperty({ type: 'integer' })
   id: number;
 
   @AutoMap()
+  @ApiProperty()
   title: string;
 
   @AutoMap()
+  @ApiProperty()
   content: string;
 
   @AutoMap()
+  @ApiProperty()
   author: UserDto;
 
+  @ApiProperty({ type: 'integer' })
   likes: number = 0;
 
+  @ApiProperty({ type: 'integer' })
   dislikes: number = 0;
 
+  @ApiProperty({ type: CommentDto })
   comments: CommentDto[];
 
+  @ApiProperty({ type: Media, isArray: true })
   media?: Media[];
 }
