ca1f50fca61a32aab39fd38bc9ffbed2ad4244db
Serghei Tibulschi <serghei@thenoughtyfox.com>
2024-04-26 18:29:33 +0300

    fix swagger

--- backend/src/infrastructure/models/dto/pagination-query.dto.ts
+++ backend/src/infrastructure/models/dto/pagination-query.dto.ts
@@ -1,11 +1,13 @@
 import { IsOptional, IsInt, Min, Max } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationQueryDto {
   @IsOptional()
   @IsInt()
   @Min(1)
   @Type(() => Number)
+  @ApiProperty({ type: 'integer', default: 1 })
   page: number = 1;
 
   @IsOptional()
@@ -13,5 +15,6 @@ export class PaginationQueryDto {
   @Min(1)
   @Max(100)
   @Type(() => Number)
+  @ApiProperty({ type: 'integer', default: 10 })
   limit: number = 10;
 }
